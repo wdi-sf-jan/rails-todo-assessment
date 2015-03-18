33e379723afb02997a31c0b0e49f2902a20e8c0d
Cynthia Yeung <cyeung91@gmail.com>
2015-03-18 08:43:13 -0700

    Added todolist_id column to todos table

--- todo_assessment/db/schema.rb
+++ todo_assessment/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318150559) do
+ActiveRecord::Schema.define(version: 20150318153603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20150318150559) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "item"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "todolist_id"
   end
 
 end
